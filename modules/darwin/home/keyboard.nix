efbd5706523e195abf5a3cad0deb918a03612e7c
Patrick Wozniak <email@patwoz.de>
2025-02-10 15:57:18 +0100

    fix keyboard

--- modules/darwin/home/keyboard.nix
+++
@@ -1,5 +0,0 @@
-{ ... }:
-
-{
-  home.file."Library/LaunchAgents/de.patwoz.KeyRemapping.plist".source = ./de.patwoz.KeyRemapping.plist;
-}
--- modules/darwin/keyboard.nix
+++ modules/darwin/keyboard.nix
@@ -5,6 +5,20 @@
     enableKeyMapping = true;
     remapCapsLockToEscape = true;
   };
+  
+  system.keyboard.userKeyMapping = [
+    # To easier enter "@"
+    {
+      # Right Command to Option
+      HIDKeyboardModifierMappingSrc = 30064771303;
+      HIDKeyboardModifierMappingDst = 30064771302;
+    }
+    {
+      # Right Option to command
+      HIDKeyboardModifierMappingSrc = 30064771302;
+      HIDKeyboardModifierMappingDst = 30064771303;
+    }
+  ];
 
   system.defaults.NSGlobalDomain = {
     AppleKeyboardUIMode = 3;

ea4d8b7f3007c37b98182947bfeccf3aecfa3fe9
Patrick Wozniak <email@patwoz.de>
2025-09-05 18:39:58 +0200

    nvim: syntax highlighting for .hurl files

--- home/nvim/lua/plugins/treesitter.lua
+++ home/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "markdown" })
+        vim.list_extend(opts.ensure_installed, { "markdown", "hurl" })
         vim.treesitter.language.register("markdown", "mdx")
       end
     end,

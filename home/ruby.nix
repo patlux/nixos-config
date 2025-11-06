3b39af3fac12faf04d9c8db9b4f00ed631b41e32
Patrick Wozniak <email@patwoz.de>
2025-11-06 19:36:20 +0100

    fix(ruby): set version for executing ruby

--- home/ruby.nix
+++ home/ruby.nix
@@ -10,7 +10,7 @@
 
   programs.zsh.initContent = "
     if [ $(mise ls ruby | wc -l) -ge 1 ]; then
-      export GEM_HOME=$(mise exec ruby --command 'ruby -e \"puts Gem.user_dir\"')
+      export GEM_HOME=$(mise exec ruby@2.7.6 --command 'ruby -e \"puts Gem.user_dir\"')
       export PATH=$PATH:$GEM_HOME/bin
     fi
   ";

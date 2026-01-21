21adb863ce201dbf14195338c41026c8df3e6582
Patrick Wozniak <email@patwoz.de>
2026-01-21 12:08:34 +0100

    fix enable backup scripts on boot
    use nix specific modules for setup lanch agents for my backup scripts

--- modules/darwin/backup/cronjob.nix
+++ modules/darwin/backup/cronjob.nix
@@ -1,10 +1,32 @@
 { ... }:
 
 {
-  # Setup backup scripts and cronjob
-  home.file."Library/LaunchAgents/de.patwoz.crontab.backuphome.plist".source = ./de.patwoz.crontab.backuphome.plist;
-  home.file."Library/LaunchAgents/de.patwoz.crontab.backupdev.plist".source = ./de.patwoz.crontab.backupdev.plist;
-
+  # Backup scripts
   home.file.".bin/backup-home.sh".source = ./backup-home.sh;
   home.file.".bin/backup-dev.sh".source = ./backup-dev.sh;
+
+  # LaunchAgents - these are automatically loaded/unloaded by home-manager
+  launchd.agents.backuphome = {
+    enable = true;
+    config = {
+      Label = "de.patwoz.crontab.backuphome";
+      ProgramArguments = [ "/Users/patwoz/.bin/backup-home.sh" ];
+      StartCalendarInterval = [{ Hour = 10; Minute = 20; }];
+      RunAtLoad = true;
+      StandardErrorPath = "/tmp/de.patwoz.crontab.backuphome.err";
+      StandardOutPath = "/tmp/de.patwoz.crontab.backuphome.out";
+    };
+  };
+
+  launchd.agents.backupdev = {
+    enable = true;
+    config = {
+      Label = "de.patwoz.crontab.backupdev";
+      ProgramArguments = [ "/Users/patwoz/.bin/backup-dev.sh" ];
+      StartCalendarInterval = [{ Hour = 10; Minute = 10; }];
+      RunAtLoad = true;
+      StandardErrorPath = "/tmp/de.patwoz.crontab.backupdev.err";
+      StandardOutPath = "/tmp/de.patwoz.crontab.backupdev.out";
+    };
+  };
 }

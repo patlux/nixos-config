b0250e33708536d58d704a5ccb32b4e5891d2751
Patrick Wozniak <email@patwoz.de>
2026-02-03 21:42:48 +0100

    fix ruby

--- home/ruby.nix
+++ home/ruby.nix
@@ -8,10 +8,9 @@
   home.file.".gemrc".source = ./files/.gemrc;
   home.file.".default-gems".source = ./files/.default-gems;
 
-  programs.zsh.initContent = "
+  programs.zsh.initContent = ''
     if [ $(mise ls ruby | wc -l) -ge 1 ]; then
-      export GEM_HOME=$(mise exec ruby@2.7.6 --command 'ruby -e \"puts Gem.user_dir\"')
-      export PATH=$PATH:$GEM_HOME/bin
+      export PATH="$(mise exec ruby -- ruby -e 'puts Gem.user_dir')/bin:$PATH"
     fi
-  ";
+  '';
 }

60a318fdbf80ff6e53899008921fa1d031d66e53
Patrick Wozniak <email@patwoz.de>
2026-02-06 22:40:21 +0100

    nvim: enable svelte

---
+++ home/nvim/lua/plugins/daily-work.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    dir = "~/dev/Personal/daily-work/daily-work.nvim",
+    ft = { "markdown" },
+    opts = {
+      ticket_urls = {
+        -- KRAN = "https://gitlab.entw.bconnect.barmer.de/.../issues/{id}",
+        -- LP = "https://app.clickup.com/t/{id}",
+        -- ENSS = "https://...",
+        -- MUSIK = "https://...",
+      },
+    },
+  },
+}
--- home/nvim/lua/plugins/disabled.lua
+++ home/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "garymjr/nvim-snippets", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false }, -- temp: vim treesitter query mismatch
 }

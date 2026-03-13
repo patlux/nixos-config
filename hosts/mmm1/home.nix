048fad64ac86f30277dc045b89b510a0d0cd1ea4
Patrick Wozniak <email@patwoz.de>
2026-03-13 18:28:36 +0100

    Add opencode and sudo to mmm1 home configuration

--- hosts/mmm1/home.nix
+++ hosts/mmm1/home.nix
@@ -10,6 +10,7 @@
     ../../home/gnupg.nix
     ../../home/mise.nix
     ../../home/nvim.nix
+    ../../home/opencode.nix
     ../../home/packages.nix
     ../../home/ruby.nix
     ../../home/shell.nix
@@ -17,7 +18,7 @@
     ../../home/wezterm.nix
   ];
 
-  programs.zsh.shellAliases.update = "darwin-rebuild switch --flake ~/.config/nixos\\#mmm1";
+  programs.zsh.shellAliases.update = "sudo darwin-rebuild switch --flake ~/.config/nixos\\#mmm1";
 
   programs.zsh.initContent = ''
     if [[ -n "$SSH_AUTH_SOCK" ]] && ! ssh-add -l >/dev/null 2>&1; then
